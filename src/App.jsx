b0aa593d7441fa174468620b175d39d20ef27456
nicolas britto <nicobritto21@gmail.com>
2023-11-15 16:31:35 -0300

    terminado

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+import { Formulario } from './component/Formulario'
+
+function App() {
+  
+
+  return (
+    <>
+      <Formulario/>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Formulario.jsx
@@ -0,0 +1,89 @@
+import { useState } from 'react'
+
+
+export const Formulario = () => {
+
+    const [nombre, setNombre] = useState('');
+    const [apellido, setApellido] = useState('');
+    const [email, setEmail] = useState('');
+    const [telefono, setTelefono] = useState('');
+    const [password, setPassword] = useState('');
+    const [password2, setPassword2] = useState('');
+
+
+
+
+    return (
+
+
+        <div className="    flex justify-center">
+
+            <form>
+
+                <div className="mb-5" >
+                    <label htmlFor="nombre" className="block text-gray-700  uppercase">Nombre</label>
+                    <input id="nombre" className="border-2 w-full p-2 mt-2 rounded-md" type="text " placeholder="Nombre"
+                        value={nombre}
+                        onChange={(a) => setNombre(a.target.value)} />
+
+                </div>
+
+                <div className="mb-5" >
+                    <label htmlFor="apellido" className="block text-gray-700  uppercase">Apellido</label>
+                    <input id="apellido" className="border-2 w-full p-2 mt-2 rounded-md" type="text " placeholder="Apellido"
+                        value={apellido}
+                        onChange={(a) => setApellido(a.target.value)} />
+
+                </div>
+
+                <div className="mb-5" >
+                    <label htmlFor="email" className="block text-gray-700  uppercase">Email</label>
+                    <input id="email" className="border-2 w-full p-2 mt-2 rounded-md" type="email" placeholder="Email"
+                        value={email}
+                        onChange={(a) => setEmail(a.target.value)} />
+
+                </div>
+
+                <div className="mb-5" >
+                    <label htmlFor="telefono" className="block text-gray-700  uppercase">Telefono</label>
+                    <input id="Telefono" className="border-2 w-full p-2 mt-2 rounded-md" type="text" placeholder="Telefono"
+                        value={telefono}
+                        onChange={(a) => setTelefono(a.target.value)} />
+
+                </div>
+
+                
+                
+                <div className="mb-5" >
+                    <label htmlFor="password" className="block text-gray-700  uppercase">Password</label>
+                    <input id="Telefono" className="border-2 w-full p-2 mt-2 rounded-md" type="password" placeholder="Password"
+                        value={password}
+                        onChange={(a) => setPassword(a.target.value)} />
+
+                </div>
+
+                <div className="mb-5" >
+                    <label htmlFor="password" className="block text-gray-700  uppercase">Confirmar Password</label>
+                    <input id="Telefono" className="border-2 w-full p-2 mt-2 rounded-md" type="password" placeholder="Password"
+                        value={password2}
+                        onChange={(a) => setPassword2(a.target.value)} />
+
+                </div>
+
+
+                <input type="submit" className="bg-indigo-600 w-full p-3 text-white 
+                    uppercase font-bold hover:bg-indigo-700 cursor-pointer"
+                    />
+
+
+
+            </form>
+
+
+        </div>
+
+
+
+
+    )
+}
